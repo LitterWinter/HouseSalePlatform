dec98319acbcd15c7c19d61d9790d502257d6baa
LitterWinter <1226179422@qq.com>
2021-01-04 23:47:10 +0800

    项目搭建

---
+++ housesale/src/main/java/com/txd/housesale/HousesaleApplication.java
@@ -0,0 +1,13 @@
+package com.txd.housesale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousesaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousesaleApplication.class, args);
+	}
+
+}
